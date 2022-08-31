56059177932b9b278d364721b02dc93234f3df85
officialpradip <study.pradipthapa@gmail.com>
2022-08-31 14:15:15 +0545

    ts

---
+++ Typescript/1.ts
@@ -0,0 +1,11 @@
+var message:string = "Hello World" 
+console.log(message)
+
+var nam:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2 
+console.log("name "+nam) 
+console.log("first score: "+score1) 
+console.log("second score: "+score2) 
+console.log("sum of the scores: "+sum)
---
+++ Typescript/2.ts
@@ -0,0 +1,29 @@
+let n=9
+
+while(n>10){
+    if(n%2==0){
+    console.log("Even")
+    
+    }else{
+    console.log("odd")
+}
+}
+
+const a =5
+do{
+    console.log("I am do while loop")
+    break
+}
+while(n>2)
+
+//
+var i:number = 1 
+while(i<=10) { 
+   if (i % 5 == 0) {   
+      console.log ("The first multiple of 5  between 1 and 10 is : "+i) 
+      break
+   //exit the loop if the first multiple is found 
+   } 
+   i++ 
+}
+console.log(i)
