fabb39651a24744041753f77866e4bcb97287d0f
officialpradip <study.pradipthapa@gmail.com>
2022-12-22 09:14:33 +0545

    Programming with js

---
+++ Programming with Javascript/week1/condition2.js
@@ -0,0 +1,14 @@
+var day ="Sunday";
+switch(day){
+    case "Sunday":
+    console.log("This is Sunday");
+    break; 
+
+    case "Monday":
+        console.log("This is Sunday");
+        break; 
+    
+        default:
+            console.log("There is not such day");
+    
+}
